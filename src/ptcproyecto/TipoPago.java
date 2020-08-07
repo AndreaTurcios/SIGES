8f26e6397c921cc22ec7bac1973b4453836c8f85
Nanos <Nanos@DESKTOP-V6LBS8T>
2020-08-07 14:53:14 -0600

    Cambio-Edwin

--- src/ptcproyecto/TipoPago.java
+++ src/ptcproyecto/TipoPago.java
@@ -33,6 +33,43 @@ public class TipoPago extends javax.swing.JInternalFrame {
     public TipoPago() {
         initComponents();
     }
+    
+    public static void main(String args[]) 
+    {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater
+        (
+            new Runnable() 
+            {
+                public void run() 
+                {
+                    new TipoPago().setVisible(true);
+                }
+            }
+        );
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
